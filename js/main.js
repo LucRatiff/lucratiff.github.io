fd37109db5166fa0016618a7ba5a85f04cc5a34b
LucRatiff <chaboule7@gmail.com>
2022-04-13 17:13:21 +0200

    edits

--- js/main.js
+++ js/main.js
@@ -2,8 +2,12 @@ const url = new URL(window.location.href);
 const params = url.searchParams;
 const page = params.get('page');
 const sectionPage = params.get('section');
-let selectionVerified = page == null;
+let documentationHomePage = page == "documentation";
+let selectionVerified = page == null || documentationHomePage;
 let title = 'Welcome';
+const isDocumentation = page != null;
+let sectionNumber = 0;
+let sectionSize = 0;
 
 const sections = {
     awesomekeys: [
@@ -63,7 +67,7 @@ if (sectionPage == null) {
     }
 }
 
-if (page != null) {
+if (isDocumentation && !documentationHomePage) {
     try {
         deploySections(page);
         document.getElementsByTagName('title').innerHTML = title;
@@ -78,11 +82,25 @@ for (let i = 0; i < parentSections.length; i++) {
     });
 }
 
-const fileName = 'https://lucratiff.github.io/resources/' + (page == null ? 'home.html' : page + '/' + sectionPage + '.html');
+if (isDocumentation) {
+	let upperpart = '<div id="upperpart"><p>Documentation</p><div id="pages">';
+	if (sectionNumber > 0) {
+		upperpart += '<a href="">&lt; previous</a>';
+	}
+	if (sectionNumber < sectionSize) {
+		upperpart += '<a href="">next &gt;</a>';
+	}
+	document.getElementById('main').innerHTML = upperpart + '</div></div>';
+} else {
+	document.body.style.backgroundColor = '#7596cc';
+}
+
+const fileName = 'https://lucratiff.github.io/resources/' +
+	(page == null ? 'home' :  page + (documentationHomePage ? '' : '/' + sectionPage)) + '.html';
 const request = new XMLHttpRequest();
 request.onreadystatechange = function() {
     if(request.readyState === 4 && (request.status === 200 || request.status == 0)) {
-        document.getElementById('content').innerHTML = request.responseText;
+        document.getElementById(isDocumentation ? 'content' : 'main').innerHTML = request.responseText;
     }
 }
 request.open("GET", fileName);
@@ -96,6 +114,7 @@ function deployBurger() {
     } else {
         document.getElementById('burgermenu').style.left = '0';
         burgerDeployed = true;
+
     }
 }
 
